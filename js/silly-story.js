e02f00550edeedf77675b949eea189b16239b2a2
chunk01 <lucamarsman@live.com>
2024-01-23 14:54:40 -0500

    Complete lab instructions

--- js/silly-story.js
+++ js/silly-story.js
@@ -5,17 +5,24 @@
 - randomize for the button
 - story for the paragraph that outputs the final story
 */
+let customName = document.getElementById("customname");
+let randomize = document.getElementsByClassName("randomize")[0];
+let story = document.getElementsByClassName("story")[0];
 
 /* STEP 3: Create the variable that contains the story string that will be modified 
 - use var storyText to containt the following:
 'It was 94 farenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but he was not surprised — :insertx: weighs 300 pounds, and it was a hot day.'
 */
+let storyText = "It was 94 farenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but he was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 
 /* STEP 4: Create three arrays, insertX, insertY, and insertZ, assigning them the following array values respectively:
 Donald Trump, Jackie Chan, Santa Claus
 Area 51, Death Valley, Aruba
 spontaneously combusted, rapidly sublimated, evaporated instantly
 */
+let insertX = ["Donald Trump", "Jackie Chan", "Santa Claus"];
+let insertY = ["Area 51", "Death Valley", "Aruba"];
+let insertZ = ["spontaneously combusted", "rapidly sublimated", "evaporated instantly"];
 
 // FUNCTIONS
 
@@ -28,35 +35,46 @@ function randomValueFromArray(array) {
 function result() {
     // STEP 7: Create a new variable called newStory and 
     //set it to the value of storyText - we don't want to overwrite the original story!
+    let newStory = storyText;
     
     /* STEP 8: Use the randomValueFromArray() function to generate a value for 
     each of three new variables - xItem, yItem, and zItem
 	Call up the function and for each variable, pass it the array from 
     which to grab a random string - for example if insertW was an array of strings, I would type:
 	var wItem = randomValueFromArray(insertW);*/
+    let xItem = randomValueFromArray(insertX);
+    let yItem = randomValueFromArray(insertY);
+    let zItem = randomValueFromArray(insertZ);
     
     //console.log(xItem);
     /* STEP 9: Replace the three placeholders in the newStory 
     string — :insertx:, :inserty:, and :insertz: — with the strings stored in 
     xItem, yItem, and zItem. Each time, be sure to update the variable newStory 
     (with =). You might need to do one of the above replacements twice! */
+    for(let i = 0; i < 2; i++){
+        newStory = newStory.replace(":insertx:", xItem);
+        newStory = newStory.replace(":inserty:", yItem);
+        newStory = newStory.replace(":insertz:", zItem);
+    }
     
-
     /* STEP 10: If the user has typed a name in the customName field, replace the name 'Bob' in the story with whatever they typed */
+    if (customName.value !== "") {
+        newStory = newStory.replace("Bob", customName.value);
+    }
     
     /* STEP 11: If the metric radio button has been checked, we need to convert the temperature and mass numbers in the story */
     if (document.getElementById("metric").checked) {
         // STEP 11a: Create a variable called weight and convert the 300lbs to kgs (1lb = 0.453592kg)
-        
+        let weight = 300 * 0.453592;
         // STEP 11b: Replace the string 300 pounds with the updated weight in kg
-        
+        newStory = newStory.replace("300 pounds", weight + " kilograms");
         // STEP 12a: Create a variable called temp and convert °F to °C ... the formula for conversion is °C = (°F - 32) x 5/9
-        
+        let temp = (94 - 32) * 5 / 9;
         // STEP 12b: Replace the string '94 fahrenheit' with the updated temperature in °C
-        
+        newStory = newStory.replace("94 farenheit", temp + " celsius");
     }
     /* STEP 13: Make the textContent property of the story variable (which references the paragraph) equal to newStory */
-    
+    story.textContent = newStory;
     // The following line makes the paragraph visible
     story.style.visibility = "visible";
 }
